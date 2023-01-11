f7ba5c6e89ce31b525b0cd3124ee2521110610f3
Celia <rubiopaiscelia@gmail.com>
2023-01-11 12:40:00 +0100

    añadir atributos

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -2,6 +2,10 @@ package witcher;
 
 public class Witcher {
 
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
