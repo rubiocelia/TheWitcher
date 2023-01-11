be33e0a65fbc8a166e9b5662c520938eb7539c1e
Celia <rubiopaiscelia@gmail.com>
2023-01-11 12:47:43 +0100

    añadidos getters y setters

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -6,6 +6,30 @@ public class Witcher {
 	private int health;
 	private boolean hasSoul;
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public boolean isHasSoul() {
+		return hasSoul;
+	}
+
+	public void setHasSoul(boolean hasSoul) {
+		this.hasSoul = hasSoul;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
